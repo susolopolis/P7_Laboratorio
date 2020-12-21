21c2fecaabea8f619d09fbbfb60c874aae61e884
susolopolis <38691897+susolopolis@users.noreply.github.com>
2020-12-21 18:58:45 +0000

    Update Passenger.java

--- src/main/java/es/ull/passengers/Passenger.java
+++ src/main/java/es/ull/passengers/Passenger.java
@@ -26,13 +26,30 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import es.ull.flights.Flight;
+/*!\brief
 
+ *Clase Passenger: Definicion y funciones asociadas
+ *
+ *\date		20 de Diciembre de 2020
+ *\author		Jesus Navarro Hernadez
+ */
 public class Passenger {
 
     private String identifier;
     private String name;
     private String countryCode;
     private Flight flight;
+
+    /*!\brief Constructor del objeto Passenger
+     *Inicializa las variables identifier, name y countryCode en funcion de las proporcionadas
+     *
+     *\param[in]	identifier  identificador del pasajero
+     *\param[in]	name    nombre del pasajero
+     * \param[in]	countryCode codigo del pais del pasajero
+     *\param[throw]	RuntimeException	Excepcion si el codigo del pais es invalido
+     *
+     *\return		Si todo va bien, nada. Si el codigo del pais es invalido (no formato ISO), una expecion
+     */
 
     public Passenger(String identifier, String name, String countryCode) {
         if (!Arrays.asList(Locale.getISOCountries()).contains(countryCode)) {
@@ -44,21 +61,61 @@ public class Passenger {
         this.countryCode = countryCode;
     }
 
+    /*!\brief Funcion getIdentifier
+     *Devuelve el identificador
+     *
+     *\param[out]	identifier  identificador del usuario
+     *
+     *\return		Identificador propio del pasajero
+     */
+
     public String getIdentifier() {
         return identifier;
     }
 
+    /*!\brief Funcion getName
+     *Devuelve el nombre
+     *
+     *\param[out]	name    nombre del pasajero
+     *
+     *\return		nombre del pasajero
+     */
     public String getName() {
         return name;
     }
+
+    /*!\brief Funcion getCountryCode
+     *Devuelve el codigo del pais
+     *
+     *\param[out]	countryCode codigo ISO del pais
+     *
+     *\return		Codigo en formato ISO del pais del pasajero
+     */
 
     public String getCountryCode() {
         return countryCode;
     }
 
+    /*!\brief Funcion getFlight
+     *Devuelve el vuelo
+     *
+     *\param[out]	flight  vuelo del pasajero
+     *
+     *\return		objeto del tipo Flight propio del pasajero
+     */
+
     public Flight getFlight() {
         return flight;
     }
+
+    /*!\brief Funcion joinFlight
+     *Establecera un nuevo vuelo para el pasajero
+     *
+     *\param[in]	flight  nuevo vuelo
+     *\param[throw]	RuntimeException    excepcion al no poder eliminar o agregar al pasajero del vuelo
+     *
+     *\return		Si todo va bien, no devolvera nada. En caso de no poder eliminar al pasajero del vuelo actualmente asignado o no poder agregarlo, una excpecion
+     */
 
     public void joinFlight(Flight flight) {
         Flight previousFlight = this.flight;
@@ -75,12 +132,28 @@ public class Passenger {
         }
     }
 
+    /*!\brief Funcion setFlight
+     *Establece como vuelo el parametro recibido
+     *
+     *\param[in]	flight  nuevo vuelo
+     *
+     */
+
     public void setFlight(Flight flight) {
         this.flight = flight;
     }
+
+    /*!\brief Funcion toString
+     *Devuelve la informacion del pasajero
+     *
+     *\param[out]	String  cadena con la informacion del pasajero
+     *
+     *\return		Cadena que contendra el nombre, identificador y pais del pasajero
+     */
 
     @Override
     public String toString() {
         return "Passenger " + getName() + " with identifier: " + getIdentifier() + " from " + getCountryCode();
     }
 }
+
